f417d11e2c942d49f1da54463a4f3247cfbd1414
arman <rman.madi@gmail.com>
2020-01-21 13:25:33 +1100

    test scroll here successfully for first time

--- src/lib/scroll-here.component.ts
+++ src/lib/scroll-here.component.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewChecked, Input } from '@angular/core';
 
 @Component({
-  selector: 'lib-scroll-here',
-  template: `
-    <p>
-      scroll-here works!
-    </p>
-  `,
-  styles: []
+  selector: 'ar-scroll-here',
+  template: '<ng-content></ng-content>',
 })
-export class ScrollHereComponent implements OnInit {
+export class ScrollHereComponent implements AfterViewChecked {
 
-  constructor() { }
+  /**
+   * Determine which parent should scrolle, if not given it will be detected automatically
+   */
+  @Input() scrollableParent: HTMLElement;
+  
+  /**
+   * offset the scroll after scrolling into the middle of the container view, default is 0
+   */
+  @Input() offset: number = 0;
 
-  ngOnInit() {
+  private isScrolled = false;
+
+  constructor(private el: ElementRef) { }
+
+  ngAfterViewChecked(): void {
+    if (!this.el.nativeElement.isConnected){
+      this.isScrolled = false;
+    
+    } else if(this.isScrolled === false) {
+
+      let scrollableParent: HTMLElement = this.scrollableParent;
+      if(!scrollableParent) {
+        scrollableParent = this.el.nativeElement.parentElement;
+        const overflowRegex = /(auto|scroll|hidden)/;
+        
+        while(scrollableParent && scrollableParent instanceof HTMLElement) {
+          let overflowValues =  
+            window.getComputedStyle(scrollableParent).overflow + 
+            window.getComputedStyle(scrollableParent).overflowY //+ 
+            // window.getComputedStyle(scrollableParent).overflowX;
+          if(scrollableParent.scrollHeight > scrollableParent.clientHeight &&
+            overflowRegex.test(overflowValues)) {
+            break;
+          }
+          scrollableParent = scrollableParent.parentElement
+        }
+
+        if(!scrollableParent || !(scrollableParent instanceof HTMLElement)) {
+          // Could not find a scrollable parent so leave it to the browser
+          this.el.nativeElement.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+          return;
+        }
+      }
+      if(scrollableParent && scrollableParent.scrollHeight > scrollableParent.clientHeight) {
+        var parentTop = scrollableParent.getBoundingClientRect().top; 
+        var childTop = this.el.nativeElement.getBoundingClientRect().top; 
+        var childParentOffset = Math.abs(parentTop - childTop)
+        scrollableParent.scrollTop = childParentOffset - Math.abs(scrollableParent.offsetHeight / 2) + parseInt(this.offset+'');
+        this.isScrolled = true;
+      }
+    }
   }
 
 }
--- src/public-api.ts
+++ src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of scroll-here
  */
 
-export * from './lib/scroll-here.service';
 export * from './lib/scroll-here.component';
 export * from './lib/scroll-here.module';
