00001b213d993c4e44291a510f2f37444a197123
arman <rman.madi@gmail.com>
2020-01-21 15:15:28 +1100

    move build folder to scroll-here itself

---
+++ dist/scroll-here/esm2015/arman.madi-scroll-here.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview added by tsickle
+ * Generated from: arman.madi-scroll-here.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { ScrollHereComponent, ScrollHereModule } from './public-api';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXJtYW4ubWFkaS1zY3JvbGwtaGVyZS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL0Bhcm1hbi5tYWRpL3Njcm9sbC1oZXJlLyIsInNvdXJjZXMiOlsiYXJtYW4ubWFkaS1zY3JvbGwtaGVyZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUlBLHNEQUFjLGNBQWMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qKlxuICogR2VuZXJhdGVkIGJ1bmRsZSBpbmRleC4gRG8gbm90IGVkaXQuXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9wdWJsaWMtYXBpJztcbiJdfQ==
